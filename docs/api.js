0cd1b7823fb2eb654ca066c041905e70a58f7e4a
Felix Nagel <info@felixnagel.com>
2015-12-28 11:58:54 +0100

    Build documentation

--- docs/api.js
+++ docs/api.js
@@ -13,6 +13,7 @@ YUI.add("yuidoc-meta", function(Y) {
             "name": "MultiDialog",
             "description": "jQuery MultiDialog Plugin"
         }
-    ]
+    ],
+    "elements": []
 } };
 });
